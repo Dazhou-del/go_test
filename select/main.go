1992d6fbe301cf1f4e8a21b4328aa5db213d9157
冉洲 <1797050390@qq.com>
2024-08-08 17:27:58 +0800

    fix

--- select/main.go
+++ select/main.go
@@ -3,13 +3,38 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"time"
 )
+
+// 向通道发送数据
+func sendData(ch chan<- int) {
+	for i := 1; i <= 5; i++ {
+		ch <- i
+		time.Sleep(time.Second) // 模拟耗时操作
+	}
+	close(ch)
+}
+
+// 从通道接收数据
+func receiveData(ch <-chan int) {
+	wg := sync.WaitGroup{}
+	wg.Add(5)
+	for num := range ch {
+		go func() {
+			defer wg.Done()
+			fmt.Println("Received:", num)
+		}()
+	}
+	//wg.Wait()
+}
 
 func main() {
 	intChannels := [3]chan int{make(chan int, 1), make(chan int, 1), make(chan int, 1)}
 
 	index := rand.Intn(3)
 	fmt.Println(index)
+	intChannels[index] <- index
 	intChannels[index] <- index
 
 	select {
@@ -24,4 +49,18 @@ func main() {
 	}
 
 	fmt.Println("No candidate case is selected!")
+
+	//dataChan := make(chan int) // 创建一个普通的双向通道
+	//
+	//// 启动goroutine发送数据到通道
+	//go sendData(dataChan)
+	//
+	//// 使用select语句从通道中接收数据
+	//select {
+	//case <-time.After(3 * time.Second):
+	//	fmt.Println("Timeout occurred. Exiting...")
+	//default:
+	//	receiveData(dataChan) // 直接调用receiveData函数，从通道中接收数据
+	//}
+
 }
