c078e46860b5f9b5e6efd607ee120d5e67e1c92a
冉洲 <1797050390@qq.com>
2024-08-08 20:43:44 +0800

    fix

--- context/WithDeadline.go
+++ context/WithDeadline.go
@@ -9,7 +9,6 @@ import (
 func main() {
 	d := time.Now().Add(3 * time.Second)
 	ctx, cancel := context.WithDeadline(context.Background(), d)
-
 	// Even though ctx will be expired, it is good practice to call its
 	// cancelation function in any case. Failure to do so may keep the
 	// context and its parent alive longer than necessary.
