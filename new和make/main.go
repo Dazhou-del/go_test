89965d444b611f124814176e7c5a15c968c44e7a
Dazhou-del <1797050390@qq.com>
2024-08-19 10:53:21 +0800

    fix

--- new和make/main.go
+++ new和make/main.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 	i := new([]int)
 	fmt.Println(i) // &[]
+	i2 := append(i, 12)
+	fmt.Println(i2)
 
 	ints := make([]int, 0, 0) // []
+	ints = append(ints, 12)
 	fmt.Println(ints)
 
 	int64s := make(chan int64)
