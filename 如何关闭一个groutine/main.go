48e56dc87f15767dea1237307ef3825225219652
Dazhou-del <1797050390@qq.com>
2024-08-19 21:55:58 +0800

    feat：goroutine

--- 如何关闭一个groutine/main.go
+++ 如何关闭一个groutine/main.go
@@ -9,13 +9,13 @@ func main() {
 	// 通过通道关闭
 	// 如果 Goroutine 中使用了阻塞操作，如 I/O 操作或者等待通道，那么关闭通道可能无法停止 Goroutine。
 	// 在这种情况下，可以使用 context.Context 来停止 Goroutine。
-	/*	done := make(chan bool)
+	/*		done := make(chan bool)
 
-		go myGoroutine(done)
+			go myGoroutine(done)
 
-		time.Sleep(time.Second * 5)
+			time.Sleep(time.Second * 5)
 
-		close(done)*/
+			close(done)*/
 
 	// 通过context关闭
 	//ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
